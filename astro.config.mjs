d783fd13a48a2e3ca913b4cbef4af68c3a3f43f7
akashindex <itz.asdf303@gmail.com>
2025-08-17 14:09:47 -0700

    abouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/static"; 
+// If you want SSR, use "@astrojs/vercel/server"
 
 export default defineConfig({
   integrations: [tailwind()],
+  output: "static", // keep static since you’re building a static site
+  adapter: vercel(),
 });
